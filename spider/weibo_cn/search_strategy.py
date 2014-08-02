7e388c1f14175ac9b8fd60f90e46b6b9748a65e2
詹昊恂 <programmer.zhx@gmail.com>
2014-08-02 20:52:24 +0800

    Implemnted a fans_page processor for testing.

---
+++ spider/weibo_cn/search_strategy.py
@@ -0,0 +1,22 @@
+from __future__ import (unicode_literals, print_function, absolute_import)
+
+import time
+from easy_spider import Strategy
+
+
+class BFS(Strategy):
+
+    def __init__(self):
+        self.queue = []
+
+    def valid(self):
+        if len(self.queue) > 0:
+            return True
+        return False
+
+    def receive_element(self, element):
+        self.queue.append(element)
+
+    def next_element(self):
+        time.sleep(0.5)
+        return self.queue.pop(0)
