e58d48a6145b842d655d6b1f424e33ae70c9aad4
zhuangjf <568987284@qq.com>
2014-08-05 11:24:17 +0800

    change to class and the result change to sturct list

--- process/lib_zjf/process.cpp
+++ process/lib_zjf/process.cpp
@@ -15,15 +15,79 @@ History    :
 #ifdef DEBUG
 int main()
 {
-	std::set<std::string> stopwordsSet;
+	std::list<std::string> IDs;
+	std::list<std::string> vec;
+	Process process;
+	std::string ID="3566050434068596";
 
-// std::cout<<RegexReplace("累死了，累死了@我是米米米米 [泪][泪][泪]还差点死在你手上！！！ 我在:http://t.cn/zYT63A2    你打击嗲   \o(*￣▽￣*)o  *()我是5.5号，JAers，我不一一@你们啦，\o(*￣▽￣*)o  *()我是5.5号，你们懂的～～[爱你]//@毛-叔: @唓哩_Yuen @佳玲Brenda @FLY--ying @西米_ \	__狠爽 @小头米非 @_LIyu 各位，小弟4月底5月初去西藏旅游，如果错过你们的毕业照，请见谅。你们是我大学非经贸学院的好朋友，希望你们能来。你们出了毕业照时间，都告诉我一声啊")<<std::endl;
 
 
-//	MakeStopSet(stopwordsSet);
-/*	Get_MIDs("2013-04-12 00:01:41",120);*/
-	std::string ID="3566051122059043";
-	Get_StringVector(ID,stopwordsSet);
+	//MakeStopSet(stopwordsSet);
+	process.Get_Stringlist(ID,vec);
+	process.Get_MIDs("2013-04-12 08:38:16",1600,IDs);
+
+
+	for(std::list<std::string>::iterator it=vec.begin() ; it!=vec.end();it++){
+				std::cout<<*it<<std::endl;
+	}
+
+
+	for(std::list<std::string>::iterator it_ids=IDs.begin();it_ids!=IDs.end();it_ids++){
+			ID=*it_ids;
+			process.Get_Stringlist(ID,vec);
+			for(std::list<std::string>::iterator it=vec.begin() ; it!=vec.end();it++){
+					std::cout<<*it<<std::endl;
+				}
+			vec.clear();
+		}
+
+
+	// example for mysql_query
+	process.set_mysql_query("select * from weibo limit 2");
+	process.Query_DB();
+	std::list<std::list<std::string> > result=process.Get_result();
+	process.display(result);
+
+/*	std::set<std::string> stopwords;
+	std::list<std::string> vec;
+	std::list<std::list<std::string> > vecs;
+	std::list<std::string> IDs;
+	MakeStopSet(stopwords);
+	std::string ID="2013-04-12 08:38:16";
+
+		std::string fenci;
+	double total;
+	char mysql_q[60];
+
+	time_t startT,endT;
+
+	IDs=Get_MIDs("2013-04-12 08:38:16",4400);
+		startT=time(NULL);
+		for(std::list<std::string>::iterator it_ids=IDs.begin();it_ids!=IDs.end();it_ids++){
+			ID=*it_ids;
+			sprintf(mysql_q,"select text from weibo where mid=%s ",ID.c_str());
+			vecs=mysql_query(mysql_q);
+			std::cout<<ID<<std::endl;
+			fenci=vecs[0][0];
+			ICTspilt(fenci.c_str());
+		//et_Stringlist(ID,stopwords,vec);
+
+
+
+
+		//	for(std::list<std::string>::iterator it=vec.begin() ; it!=vec.end();it++){
+			//		fenci.append(*it+"|");
+		//		}
+		//	insert_fenci(ID,fenci);
+
+			vec.clear();
+		 //   fenci.erase();
+		}
+
+	endT=time(NULL);
+	total=difftime(endT,startT);
+	std::cout<<"程序运行时间"<<total<<"   num"<<IDs.size()<<std::endl;
+****************************************************************************************************************/
 
 	getchar();
 	return 1;
@@ -33,20 +97,10 @@ int main()
 /**
  * 		分词函数
  */
-static std::string ICTspilt(const char * sinput){
+ std::string Process::ICTspilt(const char * sinput){
 
   std::string result;
-  //初始化
-  if(!ICTCLAS_Init())  {
 
-    printf("Init fails\n");
-	return NULL ;
-  }
-  else{
-#ifdef DEBUG
-    printf("Init ok\n");
-#endif
-  }
   //设置词性标注集(0 计算所二级标注集，1 计算所一级标注集，2 北大二级标注集，3 北大一级标注集)
   ICTCLAS_SetPOSmap(2);
 
@@ -55,71 +109,70 @@ static std::string ICTspilt(const char * sinput){
   sRst=(char *)malloc(nPaLen*6); //建议长度为字符串长度的倍。
   int nRstLen=0; //分词结果的长度
 
-  unsigned int nItems=ICTCLAS_ImportUserDictFile("userdict.txt",CODE_TYPE_GB);
-  ICTCLAS_SaveTheUsrDic();//保存用户词典
-
-
   nRstLen = ICTCLAS_ParagraphProcess(sinput,nPaLen,sRst,CODE_TYPE_UNKNOWN,0);  //字符串处理
   result=sRst;
   free(sRst);
-  ICTCLAS_Exit();	//释放资源退?
   return result;
 }
 
 
 /*****************************************************************************************
  * 		去噪分割函数
+ * 		首先是则正表达式
+ * 		然后是中文分词
+ * 		最后是停此表
  */
-static std::vector <std::string> goodWordsinPieceArticle(const std::string &rawtext,std::set<std::string> &stopwords, std::vector<std::string> &goodword){
+void Process::goodWordsinPieceArticle(std::list<std::string> &goodword){
 
-  std::vector<std::string> goodWordstemp;
-	//= ICTspilt(rawtext);
-//	boost::wregex wreg(L"\\d+");//去掉中文的空格
-  const std::string temp=RegexReplace(rawtext);  //先正则表达式
+  std::list<std::string> goodWordstemp;
+  const std::string temp=Regex_Replace(rawtext);  //先正则表达式
   std::string  result=ICTspilt(temp.c_str());
   boost::split(goodWordstemp,result ,boost::is_any_of(" "));
 
-  for(std::vector<std::string>::iterator it=goodWordstemp.begin();it!=goodWordstemp.end();it++){
+  for(std::list<std::string>::iterator it=goodWordstemp.begin();it!=goodWordstemp.end();it++){
     std::string temp=*it;
     boost::trim(temp);
-    if(!stopwords.count(temp)&&!temp.empty()){
+    if(!stopwordsSet.count(temp)&&!temp.empty()){
        goodword.push_back(temp);
 	  }
 	}
-	return goodword;
 }
 
 /***************************************************************************************************
  * 获得停词
  */
-void MakeStopSet(std::set<std::string> &stopwordsSet){
-	std::ifstream ifile("stopwords.txt");
+void Process::MakeStopSet(const char * path){
+	std::ifstream ifile(path);
+    if(!ifile){
+    	std::cout<<"打开停词表失败"<<std::endl;
+    	return ;
+    }
 	while(!ifile.eof()){
-		
       std::string temp;
 		//getline(ifile,temp);	
 	  ifile>>temp;
 	  boost::trim(temp);
 	  stopwordsSet.insert(temp);
 	}
-
 }
-
-
- std::vector<std::vector<std::string> > mysql_query(char * sql_query){
+/***************************************************************************************************
+ * mysql查询函数
+ *
+ * 查询语句在  sql_query
+ * 结果放在       result
+ *
+ */
+ void Process::Query_DB(){
 	 int res;
 	 MYSQL_RES *res_ptr;
 	 MYSQL_ROW sqlrow;
-	 MYSQL my_connection; 
-	 std::string m_str;
-	 std::vector<std::vector<std::string> > result;
+	 if(!result.empty())
+	 result.clear();
 	 mysql_init(&my_connection);     
 	 if (mysql_real_connect(&my_connection, "localhost", "", "", "test", 0, NULL, 0)){
 
-
-				
 				if ( mysql_set_character_set( & my_connection, "utf8") ) {
-					fprintf(stderr,"������ַ� %s \n",mysql_error(&my_connection));
+					fprintf(stderr,"设置中文 %s \n",mysql_error(&my_connection));
 				}
 				res=mysql_query(&my_connection,sql_query);
 				if (res) {
@@ -134,8 +187,8 @@ void MakeStopSet(std::set<std::string> &stopwordsSet){
 								{
 									printf("empty\n");
 								}  */
-								std::vector<std::string> v;
-								std::vector< std::vector<std::string> > temp;
+								std::list<std::string> v;
+								std::list< std::list<std::string> > temp;
 								result=temp;
 								if (res_ptr) {
 										while ( (sqlrow = mysql_fetch_row(res_ptr) ) ){
@@ -155,9 +208,9 @@ void MakeStopSet(std::set<std::string> &stopwordsSet){
 										if (mysql_errno(&my_connection)){
 											fprintf(stderr, "Retrive error: %s\n", mysql_error(&my_connection));  
 											}     
-										mysql_free_result(res_ptr);     
+										mysql_free_result(res_ptr);
 									 }     
-							}  
+							}
 				mysql_close(&my_connection);
 		} 
 	 else {    
@@ -167,36 +220,36 @@ void MakeStopSet(std::set<std::string> &stopwordsSet){
 					fprintf(stderr, "Connection error %d: %s\n", mysql_errno(&my_connection), mysql_error(&my_connection)); 
 				}  
 		  }
-
-	 return result;
 }
 
 /**************************************************************************
- * 显示vector二维里面的内容
+ * 显示list二维里面的内容
  */
-void display(std::vector< std::vector<std::string> > &msg)
+void Process::display(std::list< std::list<std::string> > &msg)
 {
 	
-	for(int i=0;i<msg.size();i++){
-			for(int j=0;j<msg[i].size();j++){
-					std::cout<<msg[i][j]<<std::endl;
-				}
+	for(std::list< std::list<std::string> >::iterator its=msg.begin();its!=msg.end();its++){
+		for(std::list<std::string>::iterator it=(*its).begin();it!=(*its).end();it++)
+		{
+			std::cout<<*it<<std::endl;
 		}
+
+	}
+
+
+
 }
 
-std::vector<std::string> Get_MIDs(std::string &starttimes, int seconds)
+void Process::Get_MIDs(std::string starttimes, int seconds,std::list<std::string> &MIDs)
 {
-	char  sql_query[60];
 	char  endtime[20];
-	std::vector<std::string> MIDs;
-	std::vector< std::vector<std::string> > temp;	
 	time_t t_of_day;
 	struct tm *time;
 	struct tm timetmep;
 
 	if(starttimes.empty()){
 		printf("____________________get_mids funciton imput error_____________\n");
-		return MIDs ;
+
 	}
 	timetmep=tranformTime(starttimes);
 	time=&timetmep;
@@ -204,33 +257,34 @@ std::vector<std::string> Get_MIDs(std::string &starttimes, int seconds)
 	time=localtime(&t_of_day);
 	sprintf(endtime,"%d-%d-%d %d:%d:%d",time->tm_year+1900,time->tm_mon+1,time->tm_mday,time->tm_hour,time->tm_min,time->tm_sec);
 	sprintf(sql_query,"select mid from weibo where weiboDate between '%s' and '%s' ",starttimes.c_str(),endtime);
-	temp=mysql_query( sql_query);
-
-	for(std::vector< std::vector<std::string> >::iterator iter=temp.begin();iter!=temp.end();iter++){
-			MIDs.push_back((*iter)[0]);
+	Query_DB();
+	for(std::list< std::list<std::string> >::iterator iter=result.begin();iter!=result.end();iter++){ //注意clear 是否清空到0
+			MIDs.push_back((*iter).front());
 	}
-	return MIDs;
-	
 }
 
-void Get_StringVector(std::string &mid,std::set<std::string> &stopwordsSet,std::vector<std::string> &vec){
-	char  sql_query[60];
-	std::vector< std::vector<std::string> > temp;	
+
+void Process::Get_Stringlist(std::string &mid,std::list<std::string> &vec){
+
 	if(mid.empty()){
-		printf("____________________Get_StringVector funciton imput error_____________\n");
+		printf("____________________Get_Stringlist funciton imput error_____________\n");
 		return ;
 	}
+
 	sprintf(sql_query,"select text from weibo where mid=%s ",mid.c_str());
-	temp=mysql_query( sql_query);
-	goodWordsinPieceArticle(temp[0][0],stopwordsSet,vec);
+	Query_DB();
+	rawtext=result.front().front();
+	if(!rawtext.empty())
+	goodWordsinPieceArticle(vec);
 }
 
 
 
 /**********************************************************
  * 		"2014-12-07 12:03:11" ---------------->struct tm
+ * 		时间格式转换
  */
-static struct tm tranformTime(std::string &starttime ){
+struct tm Process::tranformTime(std::string &starttime ){
 	struct tm time;
 	int start_pos=0,pos=0;
 	std::string temp[6];
@@ -254,8 +308,10 @@ static struct tm tranformTime(std::string &starttime ){
 	return time;
 }
 
-
-static  std::wstring StringToWide(std::string &sToMatch){
+/**
+ * 窄字符转换成宽字符
+ */
+std::wstring Process::StringToWide(std::string &sToMatch){
 
 	std::setlocale(LC_ALL,"zh_CN.UTF-8");
 
@@ -267,8 +323,10 @@ static  std::wstring StringToWide(std::string &sToMatch){
 	delete []lpwsz;
 	return wsToMatch;
 }
-
-static std::string WidetoString(std::wstring &wsm){
+/**
+ * 宽字符转换成窄字符
+ */
+std::string Process::WidetoString(std::wstring &wsm){
 
 		std::setlocale(LC_ALL,"zh_CN.UTF-8");
 		int iLen= wcstombs( NULL, wsm.c_str(), 0 ); // 计算转换后字符串的长度。（不包含字符串结束符）
@@ -282,35 +340,77 @@ static std::string WidetoString(std::wstring &wsm){
 
 }
 
+/**
+ * 正则表达式去噪
+ * input 需要去噪的句子
+ * output 已经去砸的句子
+ *  1 表情  如 [高兴]    								 regex：-----------------   \\[([^x00-xff]*)\\]
+ *  2 @微博 名         @traa @傅fuuuu @记得我叫青蛙忘了我叫俞映庭:  regex  -------- /?/?\@([^ ]*)[:]|/?/?\@([^ ]*)[ ]   以空格和:结尾
+ *  3 超链接    http									 regex -----------------[a-zA-z]+://[^s]*
+ *  3 一些特殊的字符              ~@#￥%%……&……                   regex ----------------[()——_*$#]*|【动画】|〜
+ */
 
 
-static std::string RegexReplace(std::string input)
-{
+
+std::string Process::Regex_Replace(std::string input){
 	std::string output;
 	std::wstring put;
 	//去掉表情
-	boost::wregex reg(L"\\[([^x00-xff]*)\\]|/?/?\@([^ ]*)[ ]|[a-zA-z]+://[^s]* ", boost::regex::perl);
+	input.append(" ");
+	boost::wregex reg(L"\\[([^x00-xff]*)\\]|/?/?\@([^ ]*)[:]|/?/?\@([^ ]*)[ ]|[a-zA-z]+://[^s]*", boost::regex::perl);
 	put=boost::regex_replace(StringToWide(input),reg,std::wstring(L""));
 	input=WidetoString(put);
 
 
 	//去掉@人名转发     思路去输掉空格                  @非空格+空格
 //	boost::wregex reg2(L"/?/?\@([^ ]*)[ ]", boost::regex::perl);
-	//匹配非数字英文中文等特殊符号
-	boost::wregex reg2(L"[()——_*$#]*", boost::regex::perl);
-	put=boost::regex_replace(StringToWide(input),reg2,std::wstring(L""));
-	input=WidetoString(put);
 
-	return input;
+
+	//匹配非数字英文中文等特殊符号
+	boost::wregex reg2(L"【动画】|[()——_*$#┮【】「┭∠」ʑ≦≧▽з؞]*|〜", boost::regex::perl);
+	put=boost::regex_replace(StringToWide(input),reg2,std::wstring(L""));
+	output=WidetoString(put);
+
+//	boost::wregex reg3(L"[^0-9a-zA-Z\u4e00-\u9fa5 ]*|〜", boost::regex::perl);
+//	put=boost::regex_replace(StringToWide(input),reg3,std::wstring(L""));
+//	output=WidetoString(put);
+
+	return output;
 	
 }
 
 
+Process::Process(){
+	  //init the ictcals
+	  if(!ICTCLAS_Init())  {
+
+	    printf("Init fails\n");
+	  }
+	  else{
+        printf("Init ok\n");
+	  }
+	  //设置词性标注集(0 计算所二级标注集，1 计算所一级标注集，2 北大二级标注集，3 北大一级标注集)
+	  ICTCLAS_SetPOSmap(2);
+
+	  ICTCLAS_ImportUserDictFile("userdict.txt",CODE_TYPE_GB);
+	  ICTCLAS_SaveTheUsrDic();//保存用户词典
+
+	  MakeStopSet("stopwords.txt");
+}
 
 
+std::list< std::list<std::string> >  Process::Get_result(void){
+	return this->result;
+}
 
 
+void Process::set_mysql_query(const char * sql){
 
+  if(sql==NULL||strlen(sql)>=100) {
+	  printf("____________________set_mysql_query funciton imput error_____________\n");
+  }
+  strcpy(sql_query,sql);
+}
 
 
 
--- process/main.cpp
+++ process/main.cpp
@@ -2,14 +2,39 @@
 #include <iostream>
 int main()
 {
-	std::set<std::string> stopwords;
-	std::vector<std::string> vec;
-	MakeStopSet(stopwords);
-	std::string ID="3566051122059043";
-	Get_StringVector(ID,stopwords,vec);
 
-	for(std::vector<std::string>::iterator it=vec.begin() ; it!=vec.end();it++)
-		std::cout<<*it<<std::endl;
+
+	/**
+	 * example for get the fenci分词
+	 */
+	std::list<std::string> IDs;
+	std::list<std::string> vec;
+	std::string ID;
+	Process process;
+
+	//this function is to get the IDs between 2013-04-12 08:38:16  and  2013-04-12 08:38:16+1600second
+	process.Get_MIDs("2013-04-12 08:38:16",1600,IDs);
+
+	for(std::list<std::string>::iterator it_ids=IDs.begin();it_ids!=IDs.end();it_ids++){
+			ID=*it_ids;
+			//this function is to get the fenci where mid=ID
+			process.Get_Stringlist(ID,vec);
+			//here is to dispaly the fenci result
+			for(std::list<std::string>::iterator it=vec.begin() ; it!=vec.end();it++){
+					std::cout<<*it<<" | "<<std::endl;
+				}
+			std::cout<<std::endl;
+			vec.clear();
+		}
+
+	/**
+	 * example for mysql_query
+	 */
+	process.set_mysql_query("select * from weibo limit 2");
+	process.Query_DB();    //to query the DB
+	std::list<std::list<std::string> > result=process.Get_result();
+	process.display(result);  //to display the mysql_query result
+
 
 	return 1;
 }	
