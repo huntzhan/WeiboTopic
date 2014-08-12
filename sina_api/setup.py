6f1674a249cff3f44605526072987e7cf310a865
詹昊恂 <programmer.zhx@gmail.com>
2014-08-12 08:32:44 +0800

    Setup project for crawling weibo through api.

---
+++ sina_api/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+# from some_interface import __version__
+
+setup(
+    name='',
+    version='0.1',
+    author='',
+    author_email='',
+    url='',
+    license='MIT',
+    description='',
+    # long_description=open('README.rst').read(),
+    install_requires=[
+        'requests>=2.3.0',
+        'sqlalchemy',
+        'futures',
+        'mysql-python',
+    ],
+    packages=[],
+    # entry_points={
+    #     'console_scripts': [
+    #         'crawl_user = weibo_com.run:crawl_user_info',
+    #     ],
+    #     "distutils.commands": [
+    #         "build_db = weibo_com.build_db:BuildDBCommand",
+    #     ],
+    # },
+    test_suite='tests.load_tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
