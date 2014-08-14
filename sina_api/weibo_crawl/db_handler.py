87ea7597291cd01e1c6c448568dfeb2ae8530aea
詹昊恂 <programmer.zhx@gmail.com>
2014-08-14 13:41:36 +0800

    Extracted irrelevant code from persist.py to db handler.py.

---
+++ sina_api/weibo_crawl/db_handler.py
@@ -0,0 +1,79 @@
+"""
+@brief: Defines db operations based on scope_session.
+"""
+from __future__ import (unicode_literals, print_function, absolute_import)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from .persist import (_WeiboUser, _User2Blog, _Microblog,
+                      DB_URL)
+
+
+engine = create_engine(
+    DB_URL,
+    pool_size=0,
+    pool_timeout=60,
+)
+
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
+
+
+def strong_filter(*required_keys, **default_pairs):
+    """
+    @brief: decorator for function of class. kwargs MUST contains all keys of
+            required_keys. default_pairs provides the default values. After
+            decorating, class function could only accept keyword arguments.
+    """
+
+    def _decorator(func):
+        """
+        @input: keys could be a single object or an iterable. This function
+                would extract corresponding values from kwargs, which should
+                be a dictionary. If kwargs do not contains such keys, the
+                function would just crash.
+        @output: when keys is an iterable, return filter dictionary; when it
+                 isn't, the function would just return the value pointed by
+                 keys.
+        """
+        def _wrap(self, **kwargs):
+            required_key_set = set(required_keys)
+            default_key_set = set(default_pairs)
+            input_key_set = set(kwargs)
+
+            if not required_key_set <= input_key_set:
+                raise RuntimeError("Missing requied keys.")
+
+            processed_kwargs = {}
+            for key in required_key_set:
+                processed_kwargs[key] = kwargs[key] or\
+                    default_pairs.get(key, None)
+
+            for key in (default_key_set - required_key_set):
+                processed_kwargs[key] = default_pairs[key]
+
+            return func(self, **processed_kwargs)
+        return _wrap
+    return _decorator
+
+
+def week_filter(*required_keys):
+    def _decorator(func):
+        def _wrap(self, **kwargs):
+            processed_kwargs = {}
+            for key, value in kwargs.items():
+                if key in required_keys:
+                    processed_kwargs[key] = value
+            return func(self, **processed_kwargs)
+        return _wrap
+    return _decorator
+
+
+class ThreadSafeHandler(object):
+
+    def open(self):
+        Session()
+
+    def close(self):
+        Session.remove()
--- sina_api/weibo_crawl/persist.py
+++ sina_api/weibo_crawl/persist.py
@@ -74,73 +74,6 @@ class _Microblog(Base):
         return "<Microblog(mid='%s')" % self.mid
 
 
-def strong_filter(*required_keys, **default_pairs):
-    """
-    @brief: decorator for function of class. kwargs MUST contains all keys of
-            required_keys. default_pairs provides the default values. After
-            decorating, class function could only accept keyword arguments.
-    """
-
-    def _decorator(func):
-        """
-        @input: keys could be a single object or an iterable. This function
-                would extract corresponding values from kwargs, which should
-                be a dictionary. If kwargs do not contains such keys, the
-                function would just crash.
-        @output: when keys is an iterable, return filter dictionary; when it
-                 isn't, the function would just return the value pointed by
-                 keys.
-        """
-        def _wrap(self, **kwargs):
-            required_key_set = set(required_keys)
-            default_key_set = set(default_pairs)
-            input_key_set = set(kwargs)
-
-            if not required_key_set <= input_key_set:
-                raise RuntimeError("Missing requied keys.")
-
-            processed_kwargs = {}
-            for key in required_key_set:
-                processed_kwargs[key] = kwargs[key] or\
-                    default_pairs.get(key, None)
-
-            for key in (default_key_set - required_key_set):
-                processed_kwargs[key] = default_pairs[key]
-
-            return func(self, **processed_kwargs)
-        return _wrap
-    return _decorator
-
-
-def week_filter(*required_keys):
-    def _decorator(func):
-        def _wrap(self, **kwargs):
-            processed_kwargs = {}
-            for key, value in kwargs.items():
-                if key in required_keys:
-                    processed_kwargs[key] = value
-            return func(self, **processed_kwargs)
-        return _wrap
-    return _decorator
-
-
-def lock_and_unlock():
-    lock = thread.allocate_lock()
-
-    def _decorator(func):
-        def _wrap(*args, **kwargs):
-            lock.acquire()
-            result = None
-            try:
-                result = func(*args, **kwargs)
-            except Exception as e:
-                print("Error in lock decorator: ", e)
-            lock.release()
-            return result
-        return _wrap
-    return _decorator
-
-
 class DatabaseHandler:
 
     @classmethod
@@ -184,9 +117,7 @@ class WeiboUserHandler(DatabaseHandler):
 
     EMPTY = -1
 
-    # @lock_and_unlock()
     @classmethod
-    # @strong_filter('uid', followees=EMPTY, fans=EMPTY, posts=EMPTY)
     def add_user(cls, uid, **kwargs):
         with cls.modify_scope() as session:
             # try:
@@ -197,9 +128,7 @@ class WeiboUserHandler(DatabaseHandler):
             user = _WeiboUser(uid=uid, **kwargs)
             session.add(user)
 
-    # @lock_and_unlock()
     @classmethod
-    # @week_filter('uid', 'followees', 'fans', 'posts')
     def update_user(cls, uid, **kwargs):
         with cls.modify_scope() as session:
             user = session.query(_WeiboUser).filter_by(uid=uid).first()
@@ -207,7 +136,6 @@ class WeiboUserHandler(DatabaseHandler):
                 setattr(user, key, value)
 
     @classmethod
-    # @strong_filter('uid')
     def user_exist(cls, uid):
         with cls.query_scope() as session:
             try:
@@ -216,7 +144,6 @@ class WeiboUserHandler(DatabaseHandler):
                 return False
             return True
 
-    # @lock_and_unlock()
     @classmethod
     @strong_filter('uid')
     def delete_user(cls, uid):
