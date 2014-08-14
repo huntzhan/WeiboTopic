8c714cc0d55cbeda263383760edba49994fe81f9
詹昊恂 <programmer.zhx@gmail.com>
2014-08-14 23:11:56 +0800

    Clean up report.

--- sina_api/weibo_crawl/statistics.py
+++ sina_api/weibo_crawl/statistics.py
@@ -7,7 +7,7 @@ from logging.handlers import SMTPHandler
 
 from sqlalchemy import func
 
-from .persist import _WeiboUser, _User2Blog, _Microblog, DatabaseHandler
+from .model_manager import ModelManager
 
 
 class ProjectSMTPHandler(SMTPHandler):
@@ -37,31 +37,15 @@ logger.addHandler(email_handler)
 logger.setLevel(logging.INFO)
 
 
-MESSAGE_TEMPLATE = (
-    b"Time: {0} - {1}"
-    b"New: WeiboUser: {2}; MicroBlog: {3}; User2Blog: {4}"
-    b"Current: WeiboUser: {5}; MicroBlog: {6}; User2Blog: {7}"
-)
-
-
 class Statistics(object):
-    size_of_weibouser = None
-    size_of_user2blog = None
-    size_of_microblog = None
 
     @classmethod
     def loadup(cls):
-        cls.size_of_weibouser = get_WeiboUser()
-        cls.size_of_microblog = get_Microblog()
-        cls.size_of_user2blog = get_User2Blog()
+        pass
 
     @classmethod
     def update_and_get_diff(cls):
-        diff_weibouser = get_WeiboUser() - cls.size_of_weibouser
-        diff_microblog = get_Microblog() - cls.size_of_microblog
-        diff_user2blog = get_User2Blog() - cls.size_of_user2blog
         cls.loadup()
-        return diff_weibouser, diff_microblog, diff_user2blog
 
     @classmethod
     def report(cls):
@@ -69,14 +53,5 @@ class Statistics(object):
         current_time = time.localtime(time.time())
         one_hour_ago = time.localtime(time.time() - 3600)
 
-        text = MESSAGE_TEMPLATE.format(
-            time.asctime(one_hour_ago),
-            time.asctime(current_time),
-            diffs[0],
-            diffs[1],
-            diffs[2],
-            cls.size_of_weibouser,
-            cls.size_of_microblog,
-            cls.size_of_user2blog,
-        )
+        text = b''
         logger.info(text.encode('utf-8'))
