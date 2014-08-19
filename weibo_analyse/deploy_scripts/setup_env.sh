7dd7ba89ac455d49e77b63d71703e1a2ea19e034
詹昊恂 <programmer.zhx@gmail.com>
2014-08-19 14:04:07 +0800

    Added script for deployment.

---
+++ weibo_analyse/deploy_scripts/setup_env.sh
@@ -0,0 +1,5 @@
+#!/bin/env bash
+alias g++48=/opt/rh/devtoolset-2/root/usr/bin/g++
+alias gcc48=/opt/rh/devtoolset-2/root/usr/bin/gcc
+export CXX=/opt/rh/devtoolset-2/root/usr/bin/g++
+export CC=/opt/rh/devtoolset-2/root/usr/bin/gcc
