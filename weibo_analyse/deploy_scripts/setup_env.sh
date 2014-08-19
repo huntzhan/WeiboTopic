81912856f18e9b044c23d92128836213a11cb784
詹昊恂 <programmer.zhx@gmail.com>
2014-08-19 15:01:33 +0800

    Changed deployment scripts.

--- weibo_analyse/deploy_scripts/setup_env.sh
+++ weibo_analyse/deploy_scripts/setup_env.sh
@@ -3,3 +3,7 @@ alias g++48=/opt/rh/devtoolset-2/root/usr/bin/g++
 alias gcc48=/opt/rh/devtoolset-2/root/usr/bin/gcc
 export CXX=/opt/rh/devtoolset-2/root/usr/bin/g++
 export CC=/opt/rh/devtoolset-2/root/usr/bin/gcc
+
+alias g++=g++48
+alias gcc=gcc48
+alias cmake=cmake28
