29c900c030ece561b9de8ca532cc823b3bd6090e
JanFan <guangyizhang.jan@gmail.com>
2014-08-20 23:08:03 +0800

    Integrate Parser into Logger, sort the key words in removed blogs by frequency [moving]

--- weibo_ad_filter/src/logger/logger.h
+++ weibo_ad_filter/src/logger/logger.h
@@ -10,12 +10,17 @@
 #include <fstream>
 #include <string>
 #include <list>
+#include <map>
+#include <utility>
 #include "db/model.h"
+#include "split/parser.h"
+#include "split/Textspilt.h"
 using std::ofstream;
 using std::ifstream;
 using std::endl;
 using std::string;
 using std::list;
+using std::map;
 
 #define SQL_ADDR "192.168.1.108"
 #define SQL_USER "root"
@@ -32,21 +37,31 @@ class Logger {
     ~Logger();
     bool HasNextBlog();
     Blog NextBlog();
-    /// void AddRemovedBlog(const Blog &b)
+    void AddRemovedBlog(const Blog &b);
+    void ProduceFinalReport();
 
   private:
-    list<Blog> cached_blogs;
+    void AskDataFromDB();
 
     ofstream out;
+    Parser parser;
+    list<Blog> cached_blogs;
+    map<string, size_t> word_count;
     const static int ROW_EACH_TIME = 1000;
     const string tablename = "Microblog1408215600";
-    int number_all_rows = 0;
-    int number_left_rows = 0;
-    /// int number_removed_rows = 0;
+    int number_all_rows = -1;
+    int number_left_rows = -1;
+    int number_removed_rows = 0;
     /// int number_spam_rows = 0;
 
     // DISALLOW_COPY_AND_ASSIGN
     Logger(const Logger&);
     void operator=(const Logger&);
+};
+
+struct sort_pair_second {
+  bool operator() (const std::pair<string, size_t> &left, const std::pair<string, size_t> &right){
+    return left.second > right.second;
+  }
 };
 
