d5fba80cfb0a9ea1db9f3db96d08715b487cec1d
haoxun <programmer.zhx@gmail.com>
2014-08-21 20:50:03 +0800

    Fixed LD_LIBRARY_PATH.

--- deploy_scripts/setup_env.sh
+++ deploy_scripts/setup_env.sh
@@ -3,7 +3,7 @@ alias g++48=/opt/rh/devtoolset-2/root/usr/bin/g++
 alias gcc48=/opt/rh/devtoolset-2/root/usr/bin/gcc
 export CXX=/opt/rh/devtoolset-2/root/usr/bin/g++
 export CC=/opt/rh/devtoolset-2/root/usr/bin/gcc
-export LD_LIBRARY_PATH=/usr/lib
+export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH
 
 alias g++=g++48
 alias gcc=gcc48
