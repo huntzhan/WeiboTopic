8fb0e078d33a5a100457fccaceb88e25ec7bd455
JanFan <guangyizhang.jan@gmail.com>
2014-08-22 15:20:23 +0800

    add SpamHandler, based on mysql_handler, which borrowed form haoxun [moving]

---
+++ weibo_ad_filter/src/database/mysql_handler.cc
@@ -0,0 +1,135 @@
+// ============================================================================
+//
+//       Filename:  mysql_handler.cc
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  08/20/2014 10:16:36
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Zhan Haoxun (huntzhan), programmer.zhx@gmail.com
+//   Organization:  
+//
+// ============================================================================
+
+#include "database/mysql_handler.h"
+
+#include <string>
+#include <memory>
+#include <utility>
+#include <sstream>
+#include <iostream>
+
+#include "mysql_driver.h"
+
+#include "cppconn/driver.h"
+#include "cppconn/exception.h"
+#include "cppconn/resultset.h"
+#include "cppconn/statement.h"
+
+using std::string;
+using std::ostringstream;
+using std::vector;
+using std::unique_ptr;
+using sql::Driver;
+using sql::Statement;
+using sql::ResultSet;
+using std::cout;
+using std::endl;
+
+namespace mysql_handler {
+
+// shared driver.
+Driver *BasicConnectionSetup::driver_ =
+  sql::mysql::get_mysql_driver_instance();
+
+
+SharedConn SimpleConnectionSetup::RetrieveConnection() const {
+  // let it crash.
+  auto *con = driver_->connect(
+      db_location_.url_,
+      db_location_.username_,
+      db_location_.password_);
+  con->setSchema(db_location_.database_);
+  return SharedConn(con);
+}
+
+
+void BasicHandler::Init() const {
+  auto new_conn = set_current_conn();
+  current_conn_ = std::move(new_conn);
+}
+
+SharedConn BasicHandler::current_conn() const {
+  return current_conn_;
+}
+
+
+SimpleHandler::SimpleHandler(const std::string &db_name,
+                             const std::string &table_name)
+    : table_name_(table_name) {
+  db_location_.url_ = "tcp://127.0.0.1:3306";
+  db_location_.username_ = "root";
+  db_location_.password_ = "123456";
+  db_location_.database_ = db_name;
+}
+
+
+SharedConn SimpleHandler::set_current_conn() const {
+  SimpleConnectionSetup conn_setup(db_location_);
+  return conn_setup.RetrieveConnection();
+}
+
+
+string SimpleHandler::table_name() const {
+  return table_name_;
+}
+
+int SpamHandler::AddSpams(vector<unsigned int> spams) {
+  auto conn = current_conn();
+  // create sql.
+  ostringstream formated_sql;
+  formated_sql << "INSERT INTO spam\n"
+               << "(fingerprint)\n"
+               << "VALUES\n";
+  for(auto ib = spams.cbegin(), ie = spams.cend(); ib != ie; ib++){
+    if(ib == ie-1)
+      formated_sql<< "(" << *ib << ")\n";  /// no comma needed for last one
+    else
+      formated_sql<< "(" << *ib << "),\n";
+  }
+  cout<<"###SQL INSERT: \n"<< formated_sql.str()<<endl;
+  // make query.
+  unique_ptr<Statement> stmt;
+  stmt.reset(conn->createStatement());
+  int update_count = stmt->executeUpdate(formated_sql.str());
+  return update_count;
+}
+
+bool SpamHandler::QuerySpamSimhash(vector<unsigned int> spam) {
+  auto conn = current_conn();
+  // create sql.
+  ostringstream formated_sql;
+  formated_sql << "SELECT EXISTS( SELECT *\n"
+               << "From " << table_name() << "\n"
+               << "WHERE \n";
+  for(auto v : spam) {
+    formated_sql<< "fingerprint = " << v << "\nOR ";
+  }
+  formated_sql<<"0)";  /// make up for ended OR
+  cout<<"###SQL QUERY: \n"<<formated_sql.str()<<endl;
+  // make query.
+  unique_ptr<Statement> stmt;
+  unique_ptr<ResultSet> res;
+  stmt.reset(conn->createStatement());
+  res.reset(stmt->executeQuery(formated_sql.str()));
+  // fetch result.
+  int exist = -1;
+  if (res->next())
+    exist = res->getInt(1);  /// first column
+  return exist;
+}
+
+}  // namespace mysql_handler
---
+++ weibo_ad_filter/src/database/mysql_handler.h
@@ -0,0 +1,107 @@
+// ============================================================================
+//
+//       Filename:  mysql_handler.h
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  08/20/2014 09:46:26
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Zhan Haoxun (huntzhan), programmer.zhx@gmail.com
+//   Organization:  
+//
+// ============================================================================
+
+#include <string>
+#include <memory>
+#include <vector>
+
+#include "mysql_driver.h"
+#include "cppconn/driver.h"
+#include "cppconn/connection.h"
+
+#ifndef _DATABASE_MYSQL_HANDLER_H_
+#define _DATABASE_MYSQL_HANDLER_H_
+namespace mysql_handler {
+
+using SharedConn = std::shared_ptr<sql::Connection>;
+
+// @brief: class to target table.
+class DatabaseLocation {
+ public:
+  std::string url_;
+  std::string username_;
+  std::string password_;
+  std::string database_;
+};
+
+
+// @brief: Setting up connection to database. Pointer to Connection object
+// would be returned as a shared_ptr, hence you should not manually free such
+// pointer.
+class BasicConnectionSetup {
+ public:
+  // @return: shared pointer to sql::Connection object configured by
+  // (url, username, password, database). 
+  virtual SharedConn RetrieveConnection() const = 0;
+  static sql::Driver *driver_;
+};
+
+
+// @brief: Connect to a specific database.
+class SimpleConnectionSetup : public BasicConnectionSetup {
+ public:
+  // constructor that use (url, username, password, database) to target db.
+  SimpleConnectionSetup(const DatabaseLocation &tl)
+      : db_location_(tl) {/* empty */};
+  // implemented interface.
+  SharedConn RetrieveConnection() const override;
+
+ private:
+  const DatabaseLocation &db_location_;
+};
+
+
+// @brief: Use BasicConnectionSetup and its derived class to open connection.
+// Then use such connection to carried out operations.
+class BasicHandler {
+ public:
+  // @brief: setup connection.
+  virtual void Init() const final;  // forbids override.
+  virtual SharedConn current_conn() const final;
+  virtual SharedConn set_current_conn() const = 0;
+
+ private:
+  mutable SharedConn current_conn_ = nullptr;
+};
+
+
+// @brief: Handler that operation on single table.
+class SimpleHandler : public BasicHandler {
+ public:
+  SimpleHandler(const std::string &db_name, const std::string &table_name);
+  SharedConn set_current_conn() const override;
+
+  std::string table_name() const;
+
+ private:
+  // default values of (url, username, password, database).
+  DatabaseLocation db_location_;
+  // name of table being operated.
+  const std::string table_name_;
+};
+
+
+class SpamHandler : public SimpleHandler {
+ public:
+  SpamHandler(const std::string &db_name, const std::string &table_name)
+      : SimpleHandler(db_name, table_name) {/* empty */}
+  std::vector<std::string> topic_for_test();
+  int AddSpams(std::vector<unsigned int> spams);
+  bool QuerySpamSimhash(std::vector<unsigned int> spam);
+};
+
+}  // namespace mysql_handler
+#endif
