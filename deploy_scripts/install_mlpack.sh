3e1bd6bcaa2516b993e378eb388439f14d4a4fa7
haoxun <programmer.zhx@gmail.com>
2014-08-22 17:15:01 +0800

    Added data mining interfaces.

--- deploy_scripts/install_mlpack.sh
+++ deploy_scripts/install_mlpack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo yum install libxml2-devel armadillo-devel
+sudo ln -s /usr/include/libxml2/libxml /usr/include/libxml 
 wget http://www.mlpack.org/files/mlpack-1.0.9.tar.gz
 tar -xf mlpack-1.0.9.tar.gz
 cd mlpack-1.0.9
