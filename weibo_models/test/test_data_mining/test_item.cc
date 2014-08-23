8648b1d58d9ec6981c055cacb1c54bb0d3c5dd63
詹昊恂 <programmer.zhx@gmail.com>
2014-08-23 15:31:22 +0800

    Adjusted test_adapter.

--- weibo_models/test/test_data_mining/test_item.cc
+++ weibo_models/test/test_data_mining/test_item.cc
@@ -19,15 +19,30 @@
 #include "utils/dimension_reducer.h"
 #include "data_mining/item_related.h"
 
+
 using utils::BitsetFeatures;
+using utils::ItemWithCosineDistance;
+
 
 TEST(test_dm, test_adapter) {
-  BitsetFeatures<10> message(~0);  // set all bits to be 1.
-  AdapterForBitset adapter(message, 3);
+  BitsetFeatures<10> all_one_message(~0);  // set all bits to be 1.
+  BitsetFeatures<10> all_zero_message(0);  // set all bits to be 0.
+  AdapterForBitset all_one_adapter(all_one_message, 1);
+  AdapterForBitset all_zero_adapter(all_zero_message, 0);
 
-  EXPECT_EQ(3, adapter.GetID());
-  auto features = adapter.GetFeatures();
-  for (const auto &value : features) {
+  EXPECT_EQ(1, all_one_adapter.GetID());
+  EXPECT_EQ(0, all_zero_adapter.GetID());
+  auto all_one_features = all_one_adapter.GetFeatures();
+  auto all_zero_features = all_zero_adapter.GetFeatures();
+  for (const auto &value : all_one_features) {
     EXPECT_EQ(1.0, value);
   }
+  for (const auto &value : all_zero_features) {
+    EXPECT_EQ(0.0, value);
+  }
+}
+
+
+test(test_dm, test_item_similarity) {
+  
 }
