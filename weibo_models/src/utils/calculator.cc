22a4077e0d30601a9d3fb89db393bfc413159d86
詹昊恂 <programmer.zhx@gmail.com>
2014-08-24 21:53:07 +0800

    Added CU calculator.

---
+++ weibo_models/src/utils/calculator.cc
@@ -0,0 +1,50 @@
+// ============================================================================
+//
+//       Filename:  calculator.cc
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  08/24/2014 21:31:06
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Zhan Haoxun (huntzhan), programmer.zhx@gmail.com
+//   Organization:  
+//
+// ============================================================================
+
+#include "utils/calculator.h"
+#include "data_mining/interface.h"
+
+
+using data_mining::ListSharedPtrItemSet;
+
+
+namespace utils {
+
+
+CatergoryUtilityEvaluator::CatergoryUtilityEvaluator(
+    const int &size, const double &sum_of_squares)
+    : size_(size), sum_of_squares_(sum_of_squares) {/* empty */}
+
+
+double CatergoryUtilityEvaluator::Evaluate(
+    const ListSharedPtrItemSet &item_sets) {
+  // CU value.
+  double catergory_utility = 0.0;
+  for (const auto &item_set : item_sets) {
+    auto features = item_set.features();
+    auto items = item_set.items();
+    // it's important to cast to double.
+    double weight = items.size() / static_cast<double>(size_);
+    // sum of suqares of features of currrent item set.
+    double sq_of_item_set = CalculateSumOfSquares(features);
+    catergory_utility += weight * (sq_of_item_set - sum_of_squares_);
+  }
+  catergory_utility /= item_sets.size();
+  return catergory_utility;
+}
+
+
+}  // namespace utils
--- weibo_models/src/utils/calculator.h
+++ weibo_models/src/utils/calculator.h
@@ -14,6 +14,8 @@
 //
 // ============================================================================
 
+#include "data_mining/interface.h"
+
 #ifndef UTILS_CALCULATOR_H_
 #define UTILS_CALCULATOR_H_
 namespace utils {
@@ -24,6 +26,20 @@ class Cosine {
   template <typename Iterable>
   static double Evaluate(const Iterable &x, const Iterable &y);
 };
+
+
+class CatergoryUtilityEvaluator {
+ public:
+  CatergoryUtilityEvaluator(const int &size, const double &sum_of_squares);
+  double Evaluate(const data_mining::ListSharedPtrItemSet &item_sets);
+
+  template <typename Iterable>
+  static double CalculateSumOfSquares(const Iterable &values);
+
+ private:
+  const int size_;
+  const double sum_of_squares_;
+}
 
 
 }  // namespace utils
