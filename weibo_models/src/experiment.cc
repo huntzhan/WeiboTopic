c9463e8999cf0ff759a53cf4153fcba980a1f128
haoxun <programmer.zhx@gmail.com>
2014-08-27 16:46:28 +0800

    Added output of experiment.cc

--- weibo_models/src/experiment.cc
+++ weibo_models/src/experiment.cc
@@ -16,6 +16,7 @@
 #include <vector>
 #include <iterator>
 #include <iostream>
+#include <string>
 
 #include "database/mysql_handler.h"
 #include "utils/dimension_reducer.h"
@@ -25,6 +26,8 @@ using std::cout;
 using std::endl;
 using std::distance;
 using std::vector;
+using std::string;
+using std::to_string;
 
 using mysql_handler::TopicHandler;
 using utils::TFIDFDimensionReducer;
@@ -34,7 +37,8 @@ using utils::VecBitset;
 constexpr const int kDimension = 30;
 
 
-vector<double> evaluate_sparse(const VecBitset &vectorized_messages) {
+vector<double> evaluate_sparse(
+    const VecBitset<kDimension> &vectorized_messages) {
   vector<double> sparse = {-1};
   int all_ones_size = 0;
 
@@ -44,7 +48,7 @@ vector<double> evaluate_sparse(const VecBitset &vectorized_messages) {
       all_ones_size += message[target_index] ? 1 : 0;
     }
     double avg_sparse = static_cast<double>(all_ones_size)
-        / (dimension * vectorized_messages.size);
+        / (dimension * vectorized_messages.size());
     sparse.push_back(avg_sparse);
   }
   return sparse;
@@ -52,19 +56,23 @@ vector<double> evaluate_sparse(const VecBitset &vectorized_messages) {
 
 
 int main() {
-  TopicHandler handler("split", "Topic_20140823_82");
-  handler.Init();
-  auto raw_messages = handler.GetMessages();
-  cout << "Got messages." << endl;
+  for (int table_id = 1; table_id != 100; ++table_id) {
+    string table_name = "Topic_20140827_" + to_string(table_id);
+    TopicHandler handler("split", table_name);
+    handler.Init();
+    auto raw_messages = handler.GetMessages();
 
-  TFIDFDimensionReducer<kDimension> reducer;
-  reducer.Process(raw_messages);
-  auto keywords = reducer.GetKeywords();
-  auto vectorized_messages = reducer.GetVectorizedMessages();
-  cout << "Vectorized messages." << endl;
+    TFIDFDimensionReducer<kDimension> reducer;
+    reducer.Process(raw_messages);
+    auto keywords = reducer.GetKeywords();
+    auto vectorized_messages = reducer.GetVectorizedMessages();
 
-  auto sparse = evaluate_sparse(vectorized_messages);
-  for (auto iter = sparse.cbegin() + 1; iter != sparse.cend(); ++iter) {
-    cout << distance(sparse.cbegin(), iter) << ": " << *iter << endl;
+    auto sparse = evaluate_sparse(vectorized_messages);
+    // cout << "Table:" << table_name << endl;
+    for (auto iter = sparse.cbegin() + 1; iter != sparse.cend(); ++iter) {
+      // cout << distance(sparse.cbegin(), iter) << ":" << *iter << endl;
+      cout << *iter << '\t';
+    }
+    cout << endl;
   }
 }
