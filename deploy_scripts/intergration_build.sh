3dbeb4863100ce912259918250f76b06187886f6
JanFan <guangyizhang.jan@gmail.com>
2014-09-02 16:17:46 +0800

    build.sh [moving]

--- deploy_scripts/intergration_build.sh
+++ deploy_scripts/intergration_build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 PROJECT_ROOT="$(dirname $(pwd))"
 
+# build data cleaner program
+cd $PROJECT_ROOT/weibo_ad_filter
+mkdir build; cd $_
+cmake ..
+make
+
 # build sub-topic program.
 cd $PROJECT_ROOT/weibo_models
 mkdir build; cd $_
